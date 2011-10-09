68a623dfb5a714f28c5cbf67663e1be9fd9f5324
Kannapiran <kanap008@gmail.com>
2011-10-09 03:02:05 -0400

    Automation Complete

--- MapThatSet/src/mapthatset/automate/AutomateSim.java
+++ MapThatSet/src/mapthatset/automate/AutomateSim.java
@@ -5,14 +5,17 @@ import mapthatset.sim.MapThatSet;
 public class AutomateSim {
 	
 	public static int QUERY_LENGTH = 0;
+	public static int QUERY_OVERLAP = 0;
 
 	public static void main(String[] args) {
 		System.out.println(" Automating ");
 		
 		MapThatSet.initialize();
-		for(int x=20; x<200; ){
-			MapThatSet.auto_main(""+x, 1, args);
-			x=x+10;
+		
+		for(int x=20; x<200; x=x+10){
+			for(QUERY_LENGTH = x/2; QUERY_LENGTH<2*x/3; QUERY_LENGTH++){
+				MapThatSet.auto_main(""+x, 1, args);
+			}
 		}
 		
 	}
