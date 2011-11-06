d0cb6fc19e00d1e2f538f8abda57fda57db4c76c
Kannapiran Thirunarayanan <kanap008@gmail.com>
2011-11-06 16:48:01 -0500

    Fixed all problems in Eat Strategy.

--- Skittles/src/skittles/g1player/EatStrategy.java
+++ Skittles/src/skittles/g1player/EatStrategy.java
@@ -17,8 +17,7 @@ public class EatStrategy {
 		info.count++;
 		int[] initialPriority = info.getPriority().getPriorityArray();
 		int[] initialPriorityForEat = info.getPriority().getInitialPriorityForEat();
-		boolean isWeightedPriorityComplete = info.getPriority().isWeightedPriorityComplete();
-		boolean complete = true;
+//		boolean isWeightedPriorityComplete = info.getPriority().isWeightedPriorityComplete();
 		
 		// decide if we do not require any more offers for all the colors - eat all 
 		// u only have the skittles u desired  to have and u do not have any other skittle
@@ -26,35 +25,54 @@ public class EatStrategy {
 		DummyMain.printArray(aintInHand, " [--- aintinhand ---] ");
 		ArrayList<Integer>  desiredVector = info.getPriority().getDesiredVector(info);
 		System.out.println(" [--- desiredVecotr ---] " + desiredVector);
-		for(int i=0 ; i<aintInHand.length; i++){
-			if(!desiredVector.contains(i) && aintInHand[i]!=-1){ // except for the desired color  check if ny other left
-				complete = false;
-			}
-		}
-		complete = info.isPlayerInactive(-1);
-		
+
+//		for(int i=0 ; i<aintInHand.length; i++){
+//			if(!desiredVector.contains(i) && aintInHand[i]!=-1){ // except for the desired color  check if ny other left
+//				complete = false;
+//			}
+//		}
+
 		//TODO decide if we dont require any  more offers for a particular color 
 		boolean isInitial = false;	
 		//if all desired colors gathered - eat one by one
-		boolean  isWeightedPriorityCompleteTemp =true;
+		boolean isWeightedPriorityCompleteTemp = true;
 		for(int i=0; i<initialPriorityForEat.length ; i++){
 			if(initialPriorityForEat[i] != -1){
-				isWeightedPriorityCompleteTemp =false;
+				isWeightedPriorityCompleteTemp = false;
 				break;
 			}
 		}
 		System.out.print("");
-	    if(complete){
-			
+		
+	    if(info.isPlayerInactive(-1) || info.isPlayerInactive(info.getIntPlayerIndex())){
+			/*
+			 * if either all players are inactive, or I am inactive, then
+			 * eat all the skittles starting from the least priority color.
+			 * If color has negative happiness, eat one by one.
+			 */
 	    	System.out.println(" >>[EatStrategy] [update] complete");
-	    	for(int i=0 ; i<aintInHand.length; i++){
-				if(aintInHand[i] != -1 && aintInHand[i] > 0){
-					aintTempEat[i] = aintInHand[i];
+	    	
+	    	for(int i=0; i<aintInHand.length; i++){
+	    		int colorIndex = initialPriority[i];
+	    		if(info.getColorHappiness(colorIndex) < 0 && aintInHand[colorIndex] > 0){
+	    			aintTempEat[colorIndex] = 1;
+	    			aintInHand[colorIndex]--;
+	    			System.out.println(" >>[EatStrategy] [update] complete - Negatives present, eating one by one - colorIndex=" + colorIndex);
+	    			checkIsRoundComplete(info);
+	    			return;
+	    		}
+	    	}
+	    	
+	    	for(int i=aintInHand.length-1 ; i>=0; i--){
+	    		int colorIndex = initialPriority[i];
+				if(info.getColorHappiness(colorIndex) >= 0 && aintInHand[colorIndex] > 0){
+					aintTempEat[colorIndex] = aintInHand[colorIndex];
 					aintInHand[i] = 0;
-					break;
+					System.out.println(" >>[EatStrategy] [update] complete - Negatives NOT present, eating all - colorIndex=" + colorIndex);
+					checkIsRoundComplete(info);
+					return;
 				}
 			}
-	    	System.out.print("");
 			
 		} else if(!isWeightedPriorityCompleteTemp){ //for initial n/2  rounds check for colors not tasted according to priority queue
 			
@@ -80,9 +98,9 @@ public class EatStrategy {
 			System.out.println(" >>[EatStrategy] [update] complete and isWeightedPriorityComplete");
 	    	
 			int eatIndex = -1;
-			double eatHappiness = 10;
+			double eatHappiness = 10000;
 			for(int i=info.getDesiredColorCount(); i<initialPriority.length; i++){
-				if(info.getColorHappiness(initialPriority[i]) >=0 && info.getColorHappiness(initialPriority[i]) < eatHappiness){
+				if(info.getColorHappiness(initialPriority[i]) >= 0 && info.getColorHappiness(initialPriority[i]) < eatHappiness){
 					if(aintInHand[initialPriority[i]] > 0){
 						eatIndex = initialPriority[i];
 						eatHappiness = info.getColorHappiness(initialPriority[i]);
@@ -91,10 +109,41 @@ public class EatStrategy {
 				}
 			}
 			
-			if(eatIndex !=-1){
+			if(eatIndex != -1){
 				aintTempEat[eatIndex] = 1;
 				aintInHand[eatIndex]--;
+				checkIsRoundComplete(info);
+				return;
 			} else {
+				
+				/*
+				 * till now what we had did is we eat a color not in 'C'. 
+				 * if we are not able to eat anything that is NOT IN 'C', then we move on the 'C'.
+				 * Now, if we have negative colors in 'C', then we eat one by one, highest negative(closest to zero) first.
+				 * Otherwise we eat the whole bunch, stating with the lowest proirity first.
+				 */
+				for(int i=0; i<aintInHand.length; i++){
+		    		int colorIndex = initialPriority[i];
+		    		if(info.getColorHappiness(colorIndex) < 0 && aintInHand[colorIndex] > 0){
+		    			aintTempEat[colorIndex] = 1;
+		    			aintInHand[colorIndex]--;
+		    			System.out.println(" >>[EatStrategy] [update] (toEat == -1) - Negatives present, eating one by one - colorIndex=" + colorIndex);
+		    			checkIsRoundComplete(info);
+		    			return;
+		    		}
+		    	}
+		    	
+		    	for(int i=aintInHand.length-1 ; i>=0; i--){
+		    		int colorIndex = initialPriority[i];
+					if(info.getColorHappiness(colorIndex) >= 0 && aintInHand[colorIndex] > 0){
+						aintTempEat[colorIndex] = aintInHand[colorIndex];
+						aintInHand[i] = 0;
+						System.out.println(" >>[EatStrategy] [update] (toEat == -1) - Negatives NOT present, eating all - colorIndex=" + colorIndex);
+						checkIsRoundComplete(info);
+						return;
+					}
+				}
+				/*
 				// changed by Erica
 				// I added in the toEat and numToEat check because the loop was running
 				// and for every skittle we had left it was adding it to the aintTempEat
@@ -110,8 +159,8 @@ public class EatStrategy {
 				int toEat = -1;
 				int numToEat = Integer.MAX_VALUE;
 				for(int  i=info.getDesiredColorCount()-1; i>=0; i--){
-					DummyMain.printArray(initialPriority, "[EatStrategy] [] initialPriority");
-					DummyMain.printArray(aintInHand, "[EatStrategy] [] aintInHand");
+					DummyMain.printArray(initialPriority, " [EatStrategy] [] initialPriority");
+					DummyMain.printArray(aintInHand, " [EatStrategy] [] aintInHand");
 					System.out.print(" [EatStrategy] [] desiredColorCount=" + info.getDesiredColorCount() + ", i=" +i);
 					System.out.println(", aintInHand[initialPriority[i]]=" + aintInHand[initialPriority[i]] + ", numToEat=" + numToEat);
 					
@@ -122,15 +171,44 @@ public class EatStrategy {
 						System.out.print("");
 					}
 				}
-			
+				
+				if(toEat == -1){
+					for(int i=0; i<aintInHand.length; i++){
+			    		int colorIndex = initialPriority[i];
+			    		if(info.getColorHappiness(colorIndex) < 0 && aintInHand[colorIndex] > 0){
+			    			aintTempEat[colorIndex] = 1;
+			    			aintInHand[colorIndex]--;
+			    			System.out.println(" >>[EatStrategy] [update] (toEat == -1) - Negatives present, eating one by one - colorIndex=" + colorIndex);
+			    			return;
+			    		}
+			    	}
+			    	
+			    	for(int i=aintInHand.length-1 ; i>=0; i--){
+			    		int colorIndex = initialPriority[i];
+						if(info.getColorHappiness(colorIndex) >= 0 && aintInHand[colorIndex] > 0){
+							aintTempEat[colorIndex] = aintInHand[colorIndex];
+							aintInHand[i] = 0;
+							System.out.println(" >>[EatStrategy] [update] (toEat == -1) - Negatives NOT present, eating all - colorIndex=" + colorIndex);
+							return;
+						}
+					}
+				}
 				aintTempEat[toEat] = aintInHand[toEat];
 				aintInHand[toEat] = 0;
-
+				*/
 			}
 			
 		}
 	    
-	    info.roundComplete = true;
+	    checkIsRoundComplete(info);
+	}
+
+	/**
+	 * checks if the round is complete after eat and updates the value to InfoBase.  
+	 * @param info
+	 */
+	private void checkIsRoundComplete(Infobase info) {
+		info.roundComplete = true;
 	    for (int i = 0; i<info.getAintInHand().length; i++){
 	    	if(info.getAintInHand()[i] != 0){
 	    		info.roundComplete = false;
